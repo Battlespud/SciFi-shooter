c2afab758fa6c7632287f516630125a27497d966
Battlespud <battlespud@hotmail.com>
2017-07-08 20:42:57 -0400

    fas

---
+++ Assets/Scripts/KeyBinds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class KeyBinds {
+
+	public static KeyCode Forward = KeyCode.W;
+	public static KeyCode Back = KeyCode.S;
+	public static KeyCode Left = KeyCode.A;
+	public static KeyCode Right = KeyCode.D;
+
+	public static KeyCode Jump = KeyCode.Space;
+
+
+}
